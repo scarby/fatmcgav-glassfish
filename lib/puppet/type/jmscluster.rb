8ff2352ff4546efdc4865aa9694023de769de097
Adam Scarborough <ascarborough@Adams-MacBook-Pro.local>
2014-11-27 14:36:18 +0000

    check for domain removed

--- lib/puppet/type/jmscluster.rb
+++ lib/puppet/type/jmscluster.rb
@@ -88,12 +88,12 @@ Puppet::Type.newtype(:jmscluster) do
   end
   
   # Autorequire the relevant domain
-  autorequire(:domain) do
-    self.catalog.resources.select { |res|
-      next unless res.type == :domain
-      res if res[:portbase] == self[:portbase]
-    }.collect { |res|
-      res[:name]
-    }
-  end
+#  autorequire(:domain) do
+#    self.catalog.resources.select { |res|
+#      next unless res.type == :domain
+#      res if res[:portbase] == self[:portbase]
+#    }.collect { |res|
+#      res[:name]
+#    }
+#  end
 end
